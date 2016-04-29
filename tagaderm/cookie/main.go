446b3aae92ab1c7d4f21724d2de8221d393aa15d
Rob Boettcher <robertb@bixly.com>
2016-04-29 13:49:21 -0700

    cookie

---
+++ tagaderm/cookie/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "github.com/nu7hatch/gouuid"
+)
+
+func main() {
+    http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+        cook, err := req.Cookie("my")
+        if err != nil {
+            id, _ := uuid.New()
+            cook = &http.Cookie{
+                Name:  "the-session",
+                Value: id.String(),
+
+                HttpOnly: true,
+            }
+            http.SetCookie(res, cook)
+        }
+        fmt.Printf("%v", cook)
+    })
+    http.ListenAndServe(":8080", nil)
+}
