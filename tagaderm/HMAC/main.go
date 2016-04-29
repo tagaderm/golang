f2722558e7ef135b960d4d12976d03ed5ccb48e0
Rob Boettcher <robertb@bixly.com>
2016-04-29 13:52:11 -0700

    hmac

--- tagaderm/HMAC/main.go
+++ tagaderm/HMAC/main.go
@@ -1,28 +1,53 @@
 package main 
 
-import(
-    "fmt"
-    "log"
-    "github.com/nu7hatch/gouuid"
+import (
+    "io"
     "net/http"
+    "fmt"
+    "crypto/hmac"
+    "crypto/sha256"
 )
 
-func foo(res http.ResponseWriter, req *http.Request){
-    cookie, err := req.Cookie("session")
-    if err != nil {
-        fmt.Fprint(res, "Cookie granted\n")
-        id, _ := uuid.NewV4()
-        cookie = &http.Cookit{
-            Name: "session"
-            Value: id.String(),
-            HttpOnly: true,
-        }
-        http.SetCookie(res, cookie)
-    }
-    fmt.Fprint(res, cookie)
+func getCode(data string) string {
+    h := hmac.New(sha256.New, []byte("ourkey"))
+    io.WriteString(h, data)
+    return fmt.Sprintf("%x", h.Sum(nil))
 }
 
-func main(){
-    http.HandleFunc("/", foo)
-    http.ListenAndServe(":8080", nil)
+func hmacaaa(res http.ResponseWriter, req *http.Request){
+    if req.URL.Path != "/" {
+        http.NotFound(res, req)
+        return
+    } 
+
+    cookie, err := req.Cookie("session-id")
+    if err != nil {
+        cookie = &http.Cookie{
+            Name:     "session-id",
+            Value:    "cookie-value",
+            HttpOnly: true,
+        }
+    }
+
+    if req.FormValue("email") != "" {
+        email := req.FormValue("email")
+        cookie.Value = email + `|` + getCode(email)
+    }
+
+    http.SetCookie(res, cookie)
+    io.WriteString(res, `<!DOCTYPE html>
+    <html>
+      <body>
+        <form method="POST">
+        `+cookie.Value+`
+          <input type="email" name="email">
+          <input type="submit">
+        </form>
+      </body>
+    </html>`)
+} 
+
+func main() {
+    http.HandleFunc("/", hmacaaa)
+    http.ListenAndServe(":8060", nil)
 }
